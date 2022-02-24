73c583ac4f87b4960b5f118f18b6e2daeda064ac
Tammy <tammycollinse@gmail.com>
2022-02-23 22:48:45 -0500

    commit tree f23c0f5c3e77ac3494e903f597d06746777d7c4f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Bob";
+
+function upperCaseCustomerName(customerName){
+  return customerName.toUpperCase();  
+}
+
+function setBestCustomer(){
+  let bestCustomer = "not bob"; 
+  return bestCustomer; 
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+ leastFavoriteCustomer = "Jesse";
+ return leastFavoriteCustomer;
+}
